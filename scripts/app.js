769cf41b5e29e5baeeb31250cf315b6ed346952b
Yousuf Islam Rathore <rathoreislam@gmail.com>
2022-05-20 15:39:17 +0500

    Update Done

---
+++ scripts/app.js
@@ -0,0 +1,14 @@
+//// Products Page ==>> Product Details Page 
+const detailPageDirector = () => {
+    window.location.assign("./productDetail.html")
+}
+
+//// Categories Page ==>> Products Page 
+const productPageDirector = () => {
+    window.location.assign("./products.html")
+}
+
+//// Add to Cart
+// const addToCartAlert = () => {
+
+// }
