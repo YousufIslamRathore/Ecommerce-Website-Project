e6b9c797fd7b1c68d4dc651fb560f8b5e8c8ae8f
Yousuf Islam Rathore <rathoreislam@gmail.com>
2022-05-23 13:30:17 +0500

    Update Done

--- scripts/app.js
+++ scripts/app.js
@@ -8,7 +8,17 @@ const productPageDirector = () => {
     window.location.assign("./products.html")
 }
 
-//// Add to Cart
-// const addToCartAlert = () => {
-
-// }
+//// Edit Bio
+const editToSave = (e) => {
+    console.log(e.innerHTML)
+    if(e.innerHTML == "EDIT") {
+        e.innerHTML = "SAVE"
+        e.previousElementSibling.disabled = false
+        e.previousElementSibling.className += " activeInput"
+    }
+    else {
+        e.innerHTML = "EDIT"
+        e.previousElementSibling.disabled = true
+        e.previousElementSibling.classList.remove("activeInput")     
+    }
+}
